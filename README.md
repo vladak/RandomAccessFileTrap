4db34b2e99a6fb326acfe7a12a7e235e5fc2fe4a
Vladimir Kotal <vlada@kotalovi.cz>
2022-05-25 09:54:52 +0200

    add base code

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# RandomAccessFileTrap
-demonstrate the pitfall of using RandomAccessFile on Windows
+# RandomAccessFile trap
+
+This repository contains code to demonstrate the pitfall of using 
+[RandomAccessFile](https://docs.oracle.com/javase/8/docs/api/java/io/RandomAccessFile.html) 
+on Windows.
+
+When using `RandomAccessFile`, the pattern usually looks like this:
+```java
+try (RandomAccessFile raf = new RandomAccessFile(file.getAbsolutePath(), "r")) {
+    FileChannel fch = raf.getChannel();
+    MappedByteBuffer buffer = fch.map(FileChannel.MapMode.READ_ONLY, 0, fch.size());
+    var data = buffer.getInt();
+}
+```
+Given the _try-with-resources_ block, the `raf` object will be closed. However, there will
+be associated resources, that will still stick around.
+This is visible in the Process Explorer (from Sysinternals) as open file handle.
+On Windows, this means that the file cannot be deleted.
+
+There is a number of related questions on Stackoverflow, 
+e.g. https://stackoverflow.com/questions/25238110/how-to-properly-close-mappedbytebuffer
+
+The point is that Java should really do better. There is [JDK-6607535](https://bugs.openjdk.java.net/browse/JDK-6607535),
+however this seems to be filed to sidestep the issue by loosening the file delete semantics rather
+than invoking the cleanup.
---
+++ src/org/trap/RandomAccessFileTrap.java
@@ -0,0 +1,70 @@
+package org.trap;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class RandomAccessFileTrap {
+
+    private static void cleanup(MappedByteBuffer buffer)
+            throws ClassNotFoundException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+        Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
+        unsafeField.setAccessible(true);
+        Object unsafe = unsafeField.get(null);
+        Method invokeCleaner = unsafeClass.getMethod("invokeCleaner", ByteBuffer.class);
+        invokeCleaner.invoke(unsafe, buffer);
+    }
+
+    /**
+     * This method uses {@link RandomAccessFile} and derived {@link FileChannel} to access the contents of a file.
+     * Specifically, it uses the {@link RandomAccessFile} with try-with-resources so that it is closed.
+     * However, on Windows this does not free all the resources so the file is still considered to be open.
+     * When a file is open on Windows by any process, it cannot be deleted. To overcome this limitation,
+     * the {@link #cleanup(MappedByteBuffer)} can be called. This works, however it relies on reflection to invoke
+     * a cleanup method from {@code sun.misc.Unsafe}. This poses a trap for those who assume that closing
+     * the {@link RandomAccessFile} is enough to free all resources associated.
+     * @param file File object
+     * @param doCleanup whether to use the workaround for closing the resources
+     * @return a String representation of the first integer in the file
+     * @throws IOException on error
+     */
+    private static String analyze(File file, boolean doCleanup) throws IOException {
+        try (RandomAccessFile raf = new RandomAccessFile(file.getAbsolutePath(), "r")) {
+            FileChannel fch = raf.getChannel();
+            MappedByteBuffer buffer = fch.map(FileChannel.MapMode.READ_ONLY, 0, fch.size());
+            String retval = String.valueOf(buffer.getInt());
+
+            if (doCleanup) {
+                try {
+                    cleanup(buffer);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return retval;
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            if (args.length != 2) {
+                System.err.println("need: <0|1> <file_path>");
+                System.exit(1);
+            }
+            System.out.println(analyze(new File(args[1]), args[0].equals("1")));
+            Files.delete(Path.of(args[0]));
+        } catch (Exception e) {
+            System.err.println("got exception: " + e);
+        }
+    }
+}
